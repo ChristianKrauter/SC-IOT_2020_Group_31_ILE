92cc62d3bc3018f5f1f3171d733823342f9b8118
Chris <chris.a.krauter@gmail.com>
2020-06-06 13:22:52 +0200

    LED indicators
    Added functionality to change indicators at chairs and entrance display
    Simplified structure of chair and entrance display objects for better addressability

--- SmartLectureHall/SmartLectureHall/Assets/Scripts/Chair.cs
+++ SmartLectureHall/SmartLectureHall/Assets/Scripts/Chair.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Seat : MonoBehaviour
+public class Chair : MonoBehaviour
 {
     private Animator anim;
+    private Transform indicator;
+    private Material red_material;
+    private Material green_material;
 
     public void LockChair()
     {
@@ -30,12 +33,24 @@ public class Seat : MonoBehaviour
 
     public void ChangeIndicator(string color = "green")
     {
-        print("chair chaded to " + color);
+        Renderer meshRenderer = indicator.GetComponent<Renderer>();
+        if (color == "green")
+        {
+            meshRenderer.material = green_material;
+        }
+        else
+        {
+            meshRenderer.material = red_material;
+        }
+        print("chair changed to " + color);
     }
 
     public void Start()
     {
+        red_material = Resources.Load<Material>("Materials/red_alert");
+        green_material = Resources.Load<Material>("Materials/green_alert");
         anim = GetComponent<Animator>();
+        indicator = transform.Find("student_indicator");
     }
 
     public void Update()
@@ -43,11 +58,13 @@ public class Seat : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             anim.ResetTrigger("unlock");
+            ChangeIndicator("red");
             LockChair();
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
             anim.ResetTrigger("lock");
+            ChangeIndicator("green");
             UnlockChair();
         }
     }
---
+++ SmartLectureHall/SmartLectureHall/Assets/Scripts/Led.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Led : MonoBehaviour
+{
+    private Material red_material;
+    private Material green_material;
+
+    public void ChangeColor(string color = "green")
+    {
+        Renderer meshRenderer = this.GetComponent<Renderer>();
+        if (color == "green")
+        {
+            meshRenderer.material = green_material;
+        }
+        else
+        {
+            meshRenderer.material = red_material;
+        }
+        print("chair changed to " + color);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        red_material = Resources.Load<Material>("Materials/red_alert");
+        green_material = Resources.Load<Material>("Materials/green_alert");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ChangeColor("red");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ChangeColor("green");
+        }
+    }
+}
