bca2fe3160160b663c38ad62751fa59f63a5dad6
Emil Czychon <st116370@stud.uni-stuttgart.de>
2020-06-11 15:30:56 +0200

    add method signatures for aiplanning

--- SmartLectureHall/SmartLectureHall/Assets/Scripts/aiPlanner.cs
+++ SmartLectureHall/SmartLectureHall/Assets/Scripts/aiPlanner.cs
@@ -7,12 +7,33 @@ public class AiPlanner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    bool openWindowTemperatureControl(){
+        return true;
+    }
+
+    bool openWindowHumidityControl()
+    {
+        return true;
+    }
+
+    bool openWindowCO2Control()
+    {
+        return true;
+    }
+
+    void openWindowControl()
+    {
+
+    }
+
+
 }
