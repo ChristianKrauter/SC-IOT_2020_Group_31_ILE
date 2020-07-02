49a3be1987338be745f5a287df0b222a30470c2c
Emil Czychon <st116370@stud.uni-stuttgart.de>
2020-07-02 15:24:48 +0200

    minor changes in air condition script

--- SmartLectureHall/SmartLectureHall/Assets/Scripts/AirConditioning.cs
+++ SmartLectureHall/SmartLectureHall/Assets/Scripts/AirConditioning.cs
@@ -5,28 +5,33 @@ public class AirConditioning : MonoBehaviour
     public Animator anim;
     public Environment env;
     private ParticleSystem ps;
-    // Start is called before the first frame update
-    public void TurnOn(float temp, float hum, float co2)
+    
+    public void TurnOn(float temp, float humidity, float co2)
     {
+		humidity = humidity; //TODO air condition changes enviroment humidity
+		co2 = co2; // TODO air condition changes enviroment co2
         anim.SetTrigger("on");
         env.airConditioning = true;
         env.airConditioningTemp = temp;
         ps.Play();
     }
 
-    // Update is called once per frame
+    
     public void TurnOff()
     {
         anim.SetTrigger("off");
         env.airConditioning = false;
         ps.Stop();
     }
+	
+	// Start is called before the first frame update
     public void Start()
     {
         ps = GetComponentInChildren<ParticleSystem>();
         ps.Stop();
     }
 
+	// Update is called once per frame
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.J))
