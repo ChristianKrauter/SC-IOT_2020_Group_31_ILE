cbede9187eec60311350383cf9239311f36ba995
Emil Czychon <st116370@stud.uni-stuttgart.de>
2020-07-06 22:33:03 +0200

    rework airQualityControl-methode

--- SmartLectureHall/SmartLectureHall/Assets/Scripts/AiPlanner.cs
+++ SmartLectureHall/SmartLectureHall/Assets/Scripts/AiPlanner.cs
@@ -14,9 +14,9 @@ public class AiPlanner : MonoBehaviour
     public AirConditioning airCon4;
 
     public float wantedTemperature = 20f; // centigrade
-    public float wantedHumidity = 0.45f; // humidity in % at the desired temperature
-    public float wantedCO2 = 0.001f; // CO2
-	
+    public float wantedHumidity = 45f; // humidity in % at the desired temperature
+    public float wantedCO2 = 0.1f; // == 0.1 % CO2
+
     public float refreshTimer = 60.0f;
     private float timer = 0.0f;
 
@@ -33,7 +33,7 @@ public class AiPlanner : MonoBehaviour
 
     private int[,] currentSeatingPlan;
     private readonly Chair[,] chairs = new Chair[19, 14];
-    
+
     public SeatDisplay display;
     private readonly Led[,] displayLEDs = new Led[19, 14];
 
@@ -104,7 +104,7 @@ public class AiPlanner : MonoBehaviour
         timer += Time.deltaTime;
         if (timer > refreshTimer)
         {
-            OpenWindowControl();            
+            OpenWindowControl();
             ApplySeatingPlan();
             UpdateSeatingDisplay();
 
@@ -120,7 +120,7 @@ public class AiPlanner : MonoBehaviour
         {
             for (int j = 0; j < 14; j++)
             {
-                if (chairs[i,j].isLocked || !occupancie[i,j])
+                if (chairs[i, j].isLocked || !occupancie[i, j])
                 {
                     displayLEDs[i, j].ChangeColor("red");
                 }
@@ -195,7 +195,7 @@ public class AiPlanner : MonoBehaviour
     void ActivateAirCondition()
     {
         print("Air conditioning activated: ");
-		
+
         airCon.TurnOn(wantedTemperature, wantedHumidity, wantedCO2);
         airCon2.TurnOn(wantedTemperature, wantedHumidity, wantedCO2);
         airCon3.TurnOn(wantedTemperature, wantedHumidity, wantedCO2);
@@ -224,32 +224,46 @@ public class AiPlanner : MonoBehaviour
 
         if (openWindowFlag[0] && openWindowFlag[1] && openWindowFlag[2])
         {
-            OpenWindows();
-            DeactivateAirCondition();
+            if (activateAirConditionFlag[0] && activateAirConditionFlag[1] && activateAirConditionFlag[2])
+            {// Control released for all Value
+                CloseWindows();
+                DeactivateAirCondition();
+            }
+            else
+            {// Window open wanted for at least one value and the rest released control
+                OpenWindows();
+                DeactivateAirCondition();
+            }        
         }
-        else 
-        {
+        else
+        {// window closed for at least one value
             CloseWindows();
         }
 
-        if (activateAirConditionFlag[0] || activateAirConditionFlag[1] || activateAirConditionFlag[2])
+        if (activateAirConditionFlag[0] && activateAirConditionFlag[1] && activateAirConditionFlag[2])
         {
-            ActivateAirCondition();
-            CloseWindows();
+            if (!openWindowFlag[0] || !openWindowFlag[1] || !openWindowFlag[2])
+            {// Window close wanted for at least one value and the rest released control
+                CloseWindows();
+                ActivateAirCondition();
+            }
+     
         }
         else
-        {
+        {// air condition for all values
             DeactivateAirCondition();
         }
 
     }
 
     //Checks if given value is in temparature range
-    bool InWantedTemperatureRange(float currentTemp) {
+    bool InWantedTemperatureRange(float currentTemp)
+    {
         float minTemp = wantedTemperature - temperatureTolerance;
         float maxTemp = wantedTemperature + temperatureTolerance;
 
-        if (currentTemp >= minTemp && currentTemp <= maxTemp) {
+        if (currentTemp >= minTemp && currentTemp <= maxTemp)
+        {
             return true;
         }
         return false;
@@ -261,37 +275,29 @@ public class AiPlanner : MonoBehaviour
         float Temp_IN = broker.GetTemperatureInside();
         float Temp_OUT = broker.GetTemperatureOutside();
 
-        //if (!InWantedTemperatureRange(Temp_IN)) //TODO: Wenn einer der Werte bereits in Range ist, öffnet sich das fenster nicht mehr 
-        //{// inner Temperature  too cold or to hot
+        //Check if opening the window makes the temperature worse
+        if ((wantedTemperature > Temp_IN && Temp_IN > Temp_OUT) /*too cold outside*/ || (Temp_OUT > Temp_IN && Temp_IN > wantedTemperature) /*too hot outside*/ )
+        {
+            //outside values too bad
 
-            //Check if opening the window makes the temperature worse
-            if ((wantedTemperature > Temp_IN && Temp_IN > Temp_OUT) /*too cold outside*/ || (Temp_OUT > Temp_IN && Temp_IN > wantedTemperature) /*too hot outside*/ )
-            {
-                //outside values too bad
+            this.activateAirConditionFlag[0] = true;
+            print("Temp: Air conditioning flag set");
+        }
+        else
+        {   //external values good enough
 
+            if (!InWantedTemperatureRange(Temp_IN))
+            {// Value is in Range but window open is not nesessary. Release window for the othe values
                 this.activateAirConditionFlag[0] = true;
-                print("Temp: Air conditioning flag set");
+                this.openWindowFlag[0] = true;
+                print("Temp: Window released for other Values");
             }
             else
-            {   //external values good enough
-
-                //if (isWindowOpen)
-                //{//Window already open, use aire condition
-                //    this.activateAirConditionFlag[0] = true;
-                //    print("Temp: Air conditioning flag set");
-                //}
-                //else
-                //{
-                    this.openWindowFlag[0] = true;
-                    print("Temp: Window flag set");
-                //}
+            {
+                this.openWindowFlag[0] = true;
+                print("Temp: Window flag set");
             }
-        //}
-        //else
-        //{
-        //    this.openWindowFlag[0] = true;
-        //    this.activateAirConditionFlag[0] = true;
-        //}
+        }
     }
 
     //Checks if given value is in humidity range
@@ -312,37 +318,29 @@ public class AiPlanner : MonoBehaviour
         float Humidity_IN = broker.GetHumidityInside();
         float Humidity_OUT = broker.GetHumidityOutside();
 
-        //if (!InWantedHumidityRange(Humidity_IN)) //TODO: Wenn einer der Werte bereits in Range ist, öffnet sich das fenster nicht mehr 
-        //{// inner Humidity too low or to high
+        //Check if opening the window makes the humidity worse
+        if ((wantedHumidity > Humidity_IN && Humidity_IN > Humidity_OUT) /*too dry air outside*/ || (Humidity_OUT > Humidity_IN && Humidity_IN > wantedHumidity) /*too hazy outside*/)
+        {
+            //outside values too bad   
 
-            //Check if opening the window makes the humidity worse
-            if ( (wantedHumidity > Humidity_IN && Humidity_IN > Humidity_OUT) /*too dry air outside*/ || (Humidity_OUT > Humidity_IN && Humidity_IN > wantedHumidity) /*too hazy outside*/)
-            {
-                //outside values too bad   
+            this.activateAirConditionFlag[1] = true;
+            print("Humidity: Air conditioning flag set");
+        }
+        else
+        {   //external values good enough
 
-                this.activateAirConditionFlag[1] = true;
-				print("Humidity: Air conditioning flag set");
+            if (!InWantedHumidityRange(Humidity_IN))
+            {// Value is in Range but window open is not nesessary. Release window for the othe values
+                this.activateAirConditionFlag[0] = true;
+                this.openWindowFlag[0] = true;
+                print("Humidity: Window released for other Values");
             }
             else
-            {   //external values good enough
-
-                //if (isWindowOpen)
-                //{//Window already open, use aire condition
-                //    this.activateAirConditionFlag[1] = true;
-                //    print("Humidity: Air conditioning flag set");
-                //}
-                //else
-                //{
-                    this.openWindowFlag[1] = true;
-                    print("Humidity: Window flag set");
-                //}
+            {
+                this.openWindowFlag[0] = true;
+                print("Humidity: Window flag set");
             }
-        //}
-        //else
-        //{
-        //    this.openWindowFlag[0] = true;
-        //    this.activateAirConditionFlag[0] = true;
-        //}
+        }
     }
 
     //Checks if given value is in CO2 range
@@ -363,37 +361,25 @@ public class AiPlanner : MonoBehaviour
         float CO2_IN = broker.GetCO2Inside();
         float CO2_OUT = broker.GetCO2Outside();
 
-        //if (InWantedCO2Range(CO2_IN)) //TODO: Wenn einer der Werte bereits in Range ist, öffnet sich das fenster nicht mehr 
-        //{// inner CO2 too low or to high
+        //Check if opening the window makes the CO2 worse
+        if ((wantedCO2 > CO2_IN && CO2_IN > CO2_OUT) /*not stuffy enough air outside*/ || (CO2_OUT > CO2_IN && CO2_IN > wantedCO2) /*too stuffy air outside*/)
+        {
+            //outside values too bad
 
-            //Check if opening the window makes the CO2 worse
-            if ( (wantedCO2 > CO2_IN && CO2_IN > CO2_OUT) /*not stuffy enough air outside*/ || (CO2_OUT > CO2_IN && CO2_IN > wantedCO2) /*too stuffy air outside*/)
-            {
-                //outside values too bad
-
-                this.activateAirConditionFlag[2] = true;
-				print("CO2: Air conditioning flag set");
-            }
-            else
-            {   //external values good enough
-
-                //if (isWindowOpen)
-                //{//Window already open, use aire condition
-                //    this.activateAirConditionFlag[2] = true;
-                //    print("Humidity: Air conditioning flag set");
-                //}
-                //else
-                //{
-                    this.openWindowFlag[2] = true;
-                    print("Humidity: Window flag set");
-                //}
-            }
-        //}
-        //else
-        //{
-        //    this.openWindowFlag[0] = true;
-        //    this.activateAirConditionFlag[0] = true;
-        //}
+            this.activateAirConditionFlag[2] = true;
+            print("CO2: Air conditioning flag set");
+        }
+        if (!InWantedCO2Range(CO2_IN))
+        {// Value is in Range but window open is not nesessary. Release window for the othe values
+            this.activateAirConditionFlag[0] = true;
+            this.openWindowFlag[0] = true;
+            print("CO2: Window released for other Values");
+        }
+        else
+        {
+            this.openWindowFlag[0] = true;
+            print("CO2: Window flag set");
+        }
     }
 
 
